721f38686c45683ce0b8b0e14c5bd55b54a312f3
mrrooobot <snapolyel@gmail.com>
2024-03-02 18:09:29 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Meu Site PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div>
+    <h1>Bem-vindo ao Meu Site PHP!</h1>
+    <p>A hora atual é: <?php echo date('H:i:s'); ?></p>
+</div>
+
+</body>
+</html>
+
