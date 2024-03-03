25eedc20026472653b01107a069758851e7f9b2c
Mr.Robot <146310351+mrrooobot@users.noreply.github.com>
2024-03-03 01:40:17 +0000

    v1

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<!DOCTYPE html>
+<?php
+// Verifica se o cookie existe
+if(isset($_COOKIE['bloqueador'])) {
+    // Se o cookie existe, exibe o conteúdo da página secreta
+} else {
+    // Se o cookie não existe, exibe erro 404
+    header("HTTP/1.0 404 Not Found");
+    exit;
+}
+?><!DOCTYPE html>
 <html>
 <head>
     <title>Meu Site PHP</title>
@@ -10,7 +19,7 @@
     <h1>Bem-vindo ao Meu Site PHP!</h1>
     <p>A hora atual é: <?php echo date('H:i:s'); ?></p>
 </div>
- <h1>tem</h1>
+ <h1>tem seu acesso liberado</h1>
 </body>
 </html>
 
